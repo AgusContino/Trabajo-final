1b927f8d5a6b95b10bac9966eb55ea06b5540ba7
AgusContino <agusconti1994@gmail.com>
2022-08-09 15:18:39 -0300

    prueba de cambios

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-/* SCRIPT */
-// Por ahora, nada por aqui.
+/* Controles de video */
+
+let videoHistoria=document.querySelector("#videoHistoria")
+let play=document.querySelector(".play")
+let pausa=document.querySelector(".pausa")
+let duracion=document.querySelector("#duracion")
+
+play.addEventListener("click",()=>{
+    videoHistoria.play()
+    duracion.textContent=`${videoHistoria.duration.toFixed(2)}`
+    console.log()
+})
+pausa.addEventListener("click",()=>{
+    videoHistoria.pause()
+})
+
+// Esto nunca funciono:
+// No pude hacer que la duracion aparezca sin antes darle play al video :(
+// duracion.textContent=`${videoHistoria.duration}`
+// Ni siquiera con console.log(videoHistoria.duration)
